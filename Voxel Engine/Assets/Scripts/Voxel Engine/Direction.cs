af0db6a11d5418d21563cb86424a1a34ca731b24
Kowl1307 <kilian.sinke@gmx.de>
2025-09-06 22:18:47 +0200

    refactored direction extensions into directions file

--- Voxel Engine/Assets/Scripts/Voxel Engine/Direction.cs
+++ Voxel Engine/Assets/Scripts/Voxel Engine/Direction.cs
@@ -1,4 +1,7 @@
-﻿namespace Voxel_Engine
+﻿using System;
+using UnityEngine;
+
+namespace Voxel_Engine
 {
     public enum Direction
     {
@@ -8,5 +11,34 @@
         Left,       //-x
         Up,         //+y
         Down        //-y
+    }
+    
+    public static class DirectionExtensions
+    {
+        public static Vector3Int GetVector3(this Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Forward => Vector3Int.forward,
+                Direction.Backwards => Vector3Int.back,
+                Direction.Right => Vector3Int.right,
+                Direction.Left => Vector3Int.left,
+                Direction.Up => Vector3Int.up,
+                Direction.Down => Vector3Int.down,
+                _ => throw new Exception("Invalid input direction")
+            };
+        }
+
+        public static Vector2Int GetVector2(this Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Right => Vector2Int.right,
+                Direction.Left => Vector2Int.left,
+                Direction.Up => Vector2Int.up,
+                Direction.Down => Vector2Int.down,
+                _ => throw new Exception("Invalid input direction")
+            };
+        }
     }
 }
