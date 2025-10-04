ee5e749060f2d0a378a8b668c43eede2b8c6156a
Kowl1307 <kilian.sinke@gmx.de>
2025-10-04 17:38:11 +0200

    added editor button to open save folder

---
+++ Voxel Engine/Assets/Scripts/Voxel Engine/Editor/WorldEditorGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Voxel_Engine.Editor
+{
+    [CustomEditor(typeof(World))]
+    public class WorldEditorGUI : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Open Save Folder"))
+            {
+                System.Diagnostics.Process.Start("explorer.exe", "/select," + Application.persistentDataPath.Replace("/","\\"));
+            }
+        }
+    }
+}
